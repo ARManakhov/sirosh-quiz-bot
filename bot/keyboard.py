ea91e52bd4360e39692d84598ebaad560963b36e
ARManakhov <sheih000@gmail.com>
2021-03-04 06:10:34 +0300

    add callback querry handler

--- bot/keyboard.py
+++ bot/keyboard.py
@@ -4,8 +4,8 @@ from telebot import types
 
 def get_command_keyboard():
     keyboard = types.InlineKeyboardMarkup()
-    keyboard.add(types.InlineKeyboardButton('Новый тест', callback_data='/new'),
-                 types.InlineKeyboardButton('Мои тесты', callback_data='/my_list'),
-                 types.InlineKeyboardButton('Пройденые тесты', callback_data='/done_list'),
-                 types.InlineKeyboardButton('O Боте', callback_data='/about'))
+    keyboard.add(types.InlineKeyboardButton('Новый тест', callback_data='new'),
+                 types.InlineKeyboardButton('Мои тесты', callback_data='my_list'),
+                 types.InlineKeyboardButton('Пройденые тесты', callback_data='done_list'),
+                 types.InlineKeyboardButton('O Боте', callback_data='about'))
     return keyboard
--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,6 @@
 import os
 import telebot
+from telebot import types
 import keyboard
 from keyboard import get_command_keyboard
 
@@ -11,12 +12,38 @@ bot = telebot.TeleBot(telegram_token)
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, reply_markup=get_command_keyboard(),
-                 text= 'Бот для создания простых тестов.\nДля создания нового теста напишите команду /new или '
-                       'отправьте ссылку на google таблцу ( пример )\n Информация оботе /about')
+                 text='Бот для создания простых тестов.\nДля создания нового теста напишите команду /new или '
+                      'отправьте ссылку на google таблцу ( пример )\n Информация оботе /about')
+
+
+@bot.callback_query_handler(func=lambda c: c.data == 'about')
+def process_callback_about(callback_query: types.CallbackQuery):
+    bot.answer_callback_query(callback_query.id)
+    bot.send_message(callback_query.from_user.id, 'Бот для создания простых тестов.\nДля создания нового теста '
+                                                        'напишите команду /new или отправьте ссылку на google таблцу '
+                                                        '( пример )\n Информация оботе /about')
+
+
+@bot.callback_query_handler(func=lambda c: c.data == 'new')
+def process_callback_new(callback_query: types.CallbackQuery):
+    bot.answer_callback_query(callback_query.id)
+    bot.send_message(callback_query.from_user.id, 'тут долно быть новое чото')
+
+
+@bot.callback_query_handler(func=lambda c: c.data == 'my_list')
+def process_callback_my_list(callback_query: types.CallbackQuery):
+   bot.answer_callback_query(callback_query.id)
+   bot.send_message(callback_query.from_user.id, 'тут долно быть новое чото')
+
+
+@bot.callback_query_handler(func=lambda c: c.data == 'done_list')
+def process_callback_done_list(callback_query: types.CallbackQuery):
+   bot.answer_callback_query(callback_query.id)
+   bot.send_message(callback_query.from_user.id, 'тут долно быть новое чото')
 
 
 @bot.message_handler(commands=['about'])
-def send_welcome(message):
+def send_about(message):
     bot.reply_to(message, reply_markup=get_command_keyboard(),
                  text='Бот создан пользователем @black_tea_party.\nИсходный код распространяется по лицензии '
                       'GPLv3.\nРепозиторий: https://github.com/ARManakhov/sirosh-quiz-bot')
